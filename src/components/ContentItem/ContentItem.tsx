d17d6cd0c92bcc114ad6fee9eaec3e2e6b7b44e6
Sanzhar Beishekeev <franki-55@mail.ru>
2024-10-31 17:08:06 +0600

    В данном компоненте отрисовывается карточка одного контента и вызывается в главном компоненте

---
+++ src/components/ContentItem/ContentItem.tsx
@@ -0,0 +1,28 @@
+import { IInfo } from '../../types';
+import * as React from 'react';
+import { Card, CardActions, CardContent, Typography } from '@mui/material';
+
+interface Props {
+  content: IInfo
+}
+
+const ContentItem: React.FC<Props> = ({content}) => {
+  return (
+    <>
+      <Card sx={{ width: "100%" }}>
+        <CardContent>
+          <Typography gutterBottom sx={{ fontSize: 20 }}>
+            {content.title}
+          </Typography>
+        </CardContent>
+        <CardActions sx={{display: 'flex', flexDirection: 'column'}}>
+          <Typography gutterBottom sx={{ fontSize: 20 }}>
+            {content.content}
+          </Typography>
+        </CardActions>
+      </Card>
+    </>
+  );
+};
+
+export default ContentItem;
