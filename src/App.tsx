1566317a544e06d59e02284bb2864ef6c1ceb00c
Sanzhar Beishekeev <franki-55@mail.ru>
2024-10-31 17:09:54 +0600

    Сделал роутинг

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,24 @@
+import { Route, Routes } from 'react-router-dom';
+import Pages from './container/Pages/Pages.tsx';
+import { Container } from '@mui/material';
+import Navbar from './components/Navbar/Navbar.tsx';
+import AdminForm from "./components/AdminForm/AdminForm.tsx";
+
+
 const App = () => {
   return (
     <>
+      <header>
+        <Navbar/>
+      </header>
+      <Container>
+        <Routes>
+          <Route path="/pages/:contentPage" element={<Pages/>}/>
+          <Route path='/' element={<Pages/>}/>
+          <Route path='/pages/Admin' element={<AdminForm/>}/>
+          <Route path="*" element={<h1>Not found</h1>} />
+        </Routes>
+      </Container>
     </>
   );
 };
