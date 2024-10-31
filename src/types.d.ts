89c52065d9a301f60cf0696ab6f66851c5a3b3b9
Sanzhar Beishekeev <franki-55@mail.ru>
2024-10-31 17:08:48 +0600

    Глобальная типизация данных

---
+++ src/types.d.ts
@@ -0,0 +1,17 @@
+
+export interface IInfoForm {
+  title: string;
+  content: string;
+  category?: string;
+}
+
+export interface IInfo {
+  id: string;
+  title: string;
+  content: string;
+  category?: string;
+}
+
+export interface IInfoAPI {
+  [id: string]: IInfo;
+}
